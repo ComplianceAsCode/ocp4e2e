42f6f602d88475b39e4a6ee4f1b07315e79be5a3
Lance Bragstad <lbragsta@redhat.com>
2025-07-22 12:09:24 -0500

    Add operator namespace and cluster version to test config
    These are useful bits of information we use across the client and tests.
    Let's add them to the config so we can share them.

--- config/config.go
+++ config/config.go
@@ -1,9 +1,15 @@
 package config
 
 import (
+	goctx "context"
 	"flag"
 	"fmt"
+	"log"
+	"os/exec"
+	"regexp"
 	"time"
+
+	"k8s.io/apimachinery/pkg/types"
 )
 
 // TestConfig holds all the configuration arguments for the test suite
@@ -20,6 +26,8 @@ type TestConfig struct {
 	InstallOperator       bool
 	BypassRemediations    bool
 	TestType              string
+	OperatorNamespace     types.NamespacedName
+	Version               string
 }
 
 var (
@@ -37,6 +45,10 @@ var (
 // default values for the flags that are not provided. It's safe to call this
 // multiple times.
 func NewTestConfig() *TestConfig {
+	version, err := setVersion()
+	if err != nil {
+		log.Fatalf("failed to set version: %s", err)
+	}
 	return &TestConfig{
 		APIPollInterval:       5 * time.Second,
 		E2eSettings:           "e2e-debug",
@@ -49,6 +61,8 @@ func NewTestConfig() *TestConfig {
 		InstallOperator:       installOperator,
 		BypassRemediations:    bypassRemediations,
 		TestType:              testType,
+		OperatorNamespace:     types.NamespacedName{Name: "compliance-operator"},
+		Version:               version,
 	}
 }
 
@@ -75,4 +89,22 @@ func ValidateFlags() error {
 		return fmt.Errorf("a product must be given with the `-product` flag")
 	}
 	return nil
+}
+
+func setVersion() (string, error) {
+	// TODO(jaosorior): Make this pluggable (we might want to use
+	//                  kubectl instead in the future)
+	ctx := goctx.Background()
+	rawversion, err := exec.CommandContext(ctx, "oc", "version").Output()
+	if err != nil {
+		return "", fmt.Errorf("failed get cluster version: %w", err)
+	}
+
+	r := regexp.MustCompile(`Server Version: ([1-9]\.[0-9]+)\..*`)
+	matches := r.FindSubmatch(rawversion)
+
+	if len(matches) < 2 {
+		return "", fmt.Errorf("couldn't get server version from output: %s", rawversion)
+	}
+	return string(matches[1]), nil
 }
