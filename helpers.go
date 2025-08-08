aea20397fa1b774f1b36cff6d6b04b8fc3d2b1a4
Lance Bragstad <lbragsta@redhat.com>
2025-08-08 13:29:13 -0500

    Remove duplicate flag definitions
    This is breaking tests because the flags are defined twice.

--- helpers.go
+++ helpers.go
@@ -1,8 +1,6 @@
 package ocp4e2e
 
 import (
-	"flag"
-
 	"k8s.io/apimachinery/pkg/types"
 )
 
@@ -17,16 +15,6 @@ type RuleTestResults struct {
 	RuleResults map[string]RuleTest `yaml:"rule_results"`
 }
 
-var (
-	product            string
-	profile            string
-	platform           string
-	contentImage       string
-	installOperator    bool
-	bypassRemediations bool
-	testType           string
-)
-
 type e2econtext struct {
 	// These are public because they're needed in the template
 	Profile                string
@@ -37,13 +25,4 @@ type e2econtext struct {
 }
 
 func init() {
-	flag.StringVar(&profile, "profile", "", "The profile to check")
-	flag.StringVar(&product, "product", "", "The product this profile is for - e.g. 'rhcos4', 'ocp4'")
-	flag.StringVar(&platform, "platform", "ocp4", "The platform that the tests are running on - e.g. 'ocp4', 'rosa'")
-	flag.StringVar(&contentImage, "content-image", "", "The path to the image with the content to test")
-	flag.BoolVar(&installOperator, "install-operator", true, "Should the test-code install the operator or not? "+
-		"This is useful if you need to test with your own deployment of the operator")
-	flag.BoolVar(&bypassRemediations, "bypass-remediations", false,
-		"Do not apply remedations and summarize results after the first scan")
-	flag.StringVar(&testType, "test-type", "all", "Type of rules to test: 'platform', 'node', or 'all' (default)")
 }
